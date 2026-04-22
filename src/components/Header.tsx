15b87e46efedbe36c6d53b7f6df4e2c23343d083
Jota <ferramentastech.apps@gmail.com>
2026-04-21 22:33:55 -0300

    Ship update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ export function Header() {
                 <img
                   src="/logo2.png"
                   alt="Logo"
-                  className="h-10 sm:h-14 w-auto"
+                  className="h-16 sm:h-20 w-auto"
                 />
               </motion.a>
 
