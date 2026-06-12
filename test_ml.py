4fb23614b1fca58944f3b69b2e75d28b5baaeeae
ferramentastechapps <apkassistenciatecnica@gmail.com>
2026-06-12 16:45:28 -0300

    Test ML items API

--- test_ml.py
+++ test_ml.py
@@ -4,6 +4,6 @@ sys.path.append('bot')
 from scraper_ml import MercadoLivreAPIScraper
 
 s = MercadoLivreAPIScraper()
-r = s._request('GET', 'https://api.mercadolibre.com/sites/MLB/search?category=MLB1798&limit=1')
+r = s._request('GET', 'https://api.mercadolibre.com/items/MLB4620021666')
 print(r.status_code)
 print(r.text)
