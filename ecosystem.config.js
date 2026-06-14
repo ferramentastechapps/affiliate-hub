f4031274b41158199ace1e92d82139f73ea068c5
ferramentastechapps <apkassistenciatecnica@gmail.com>
2026-06-14 19:52:24 -0300

    Add nextjs to ecosystem.config.js and clean up old PM2 processes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   apps: [
     {
+      name: 'nextjs',
+      script: 'npm',
+      args: 'start -- -p 3005',
+      watch: false,
+    },
+    {
       name: 'affiliate-hub-listener',
       script: './bot/telegram_listener.py',
       interpreter: 'python3',
